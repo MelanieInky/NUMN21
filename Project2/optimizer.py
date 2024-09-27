51772bce23b2100cc1c6d92787d3cbdcf629674d
wkuanteh <wankuanteh@gmail.com>
2024-09-27 10:01:48 +0200

    Added the 5 classes

--- Project2/optimizer.py
+++ Project2/optimizer.py
@@ -69,3 +69,135 @@ if __name__ == '__main__':
     print(f'optimizer.xhist: {optimizer.xhist}')
 
     #blabkla
+
+class GoodBroyden(Optimizer):
+    def calculate_H(self, H, gnew, g, xnew, x):
+        """
+        # Parameters:
+
+        - H: Inverse Hessian 
+        - g: gradient of objective funtion at the previous point x
+        - gnew: gradient of objective funtion at the new point xnew
+        - x: The current/previous point in the parameter space before the update
+        - xnew: The new point in the parameter space
+        """
+        d = xnew - x    #displacement between the new point xnew and the old point x
+        y = gnew - g    #difference between in gradients between the new point xnew and the old point x 
+        d = np.reshape(d, (d.shape[0], 1))
+        y = np.reshape(y, (y.shape[0], 1))
+
+        Hnew = H + (d - H @ y) / (d.T @ H @ y) @ d.T @ H    #update Inverse Hessian by Sherman-Morisson formula
+        return Hnew
+
+
+class BadBroyden(Optimizer):
+    def calculate_H(self, H, gnew, g, xnew, x):
+        """
+        # Parameters:
+
+        - H: Inverse Hessian
+        - g: gradient of objective funtion at the previous point x
+        - gnew: gradient of objective funtion at the new point xnew
+        - x: The current/previous point in the parameter space before the update
+        - xnew: The new point in the parameter space
+        """
+        d = xnew - x    #displacement between the new point xnew and the old point x
+        y = gnew - g    #difference between in gradients between the new point xnew and the old point x
+        d = np.reshape(d, (d.shape[0], 1))
+        y = np.reshape(y, (y.shape[0], 1))
+
+        Hnew = H + (d - H @ y) / (y.T @ y) @ y.T #update
+
+        return Hnew
+    
+class SymmetricBroyden(Optimizer):
+    def calculate_H(self, H, gnew, g, xnew, x):
+        """
+        # Parameters:
+
+        - H: Inverse Hessian
+        - g: gradient of objective funtion at the previous point x
+        - gnew: gradient of objective funtion at the new point xnew
+        - x: The current/previous point in the parameter space before the update
+        - xnew: The new point in the parameter space
+        """
+        d = xnew - x    #displacement between the new point xnew and the old point x
+        y = gnew - g    #difference between in gradients between the new point xnew and the old point x
+
+        d = np.reshape(d, (d.shape[0], 1))
+        y = np.reshape(y, (y.shape[0], 1))
+
+        u = d - H @ y   #difference between displacement d and the predicted change based on the current Hessian approximation
+        a = 1 / (u.T @ y)   #the reciprocal the dot product of the vector u with the gradient change y
+
+        Hnew = H + a * u @ u.T  #update
+
+        return Hnew
+
+class DFP(Optimizer):
+    def calculate_H(self, H, gnew, g, xnew, x):
+        """
+        # Parameters:
+
+        - H: Inverse Hessian
+        - g: gradient of objective funtion at the previous point x
+        - gnew: gradient of objective funtion at the new point xnew
+        - x: The current/previous point in the parameter space before the update
+        - xnew: The new point in the parameter space
+        """
+        d = xnew - x    #displacement between the new point xnew and the old point x
+        y = gnew - g    #difference between in gradients between the new point xnew and the old point x
+        d = np.reshape(d, (d.shape[0], 1))
+        y = np.reshape(y, (y.shape[0], 1))
+
+        Hnew = H + (d @ d.T) / (d.T @ y) - (H @ y @ y.T @ H) / (y.T @ H @ y)    #update
+        return Hnew        
+    
+class BFGS(Optimizer):
+    def calculate_H(self, H, gnew, g, xnew, x):
+        d = xnew - x    #displacement between the new point xnew and the old point x
+        y = gnew - g    #difference between in gradients between the new point xnew and the old point x
+        d = np.reshape(d, (d.shape[0], 1))
+        y = np.reshape(y, (y.shape[0], 1))
+
+        #dTy = d.T @ y
+        #dyT = d @ y.T
+
+        Hnew = (
+            H
+            + (1 + (y.T @ H @ y) / d.T @ y) * (d @ d.T) / d.T @ y
+            - (d @ y.T @ H + H @ y @ d.T) / (d.T @ y)
+        )
+
+        return Hnew
+
+    def optimize(self, x0):
+        x_list = [x0]
+        n = x0.shape[0]
+        xnew = x0
+        gnew = self.problem.gradient_function(x0)
+        Hnew = np.eye(n)
+        # self.points.append(copy.deepcopy(xnew))
+
+        for _ in range(self.max_iterations):
+            x = xnew
+            g = gnew
+            H = Hnew
+            # print(H)
+
+            s = -Hnew @ gnew
+            # alpha, *_ = self.line_search.search(x, s, 0, 1e8)
+            alpha, *_ = self.line_search.search(x, s)
+
+            xnew = x + alpha * s
+            gnew = self.problem.gradient_function(xnew)
+            Hnew = self.calculate_H(H, gnew, g, xnew, x)
+            x_list.append(xnew)
+
+            # self.points.append(copy.deepcopy(xnew))
+            if self.check_criterion(x, xnew, g):
+                self.success = True
+                self.xmin = xnew
+                break
+
+        return x_list  
